8c0a55afd24c21e0a241ee41613ca2a892e4b995
Jonathan Lemos <jonathanlemos@protonmail.com>
2022-12-01 19:47:30 -0500

    day1-b

--- src/advent_of_code_2022/core.clj
+++ src/advent_of_code_2022/core.clj
@@ -6,3 +6,9 @@
         ints (map #(map read-string %) groups)
         sums (map utils/sum ints)]
     (reduce max sums)))
+
+(defn day1-b [x]
+  (let [groups (utils/input-newline-separated-groups x)
+        ints (map #(map read-string %) groups)
+        sums (map utils/sum ints)]
+    (take 3 (reverse (sort sums)))))
