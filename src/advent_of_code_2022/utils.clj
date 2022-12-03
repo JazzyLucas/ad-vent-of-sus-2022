c4004647d1c5121d4656799a1a2395d2b0422f44
Jonathan Lemos <jonathanlemos@protonmail.com>
2022-12-03 00:42:12 -0500

    day 3

--- src/advent_of_code_2022/utils.clj
+++ src/advent_of_code_2022/utils.clj
@@ -25,3 +25,24 @@
 (def line-fields (comp #(map split-whitespace %) lines))
 
 (def input-line-fields (comp line-fields get-input))
+
+(defn between [a b x]
+  (if (> a b)
+    (between b a x)
+    (and (>= x a) (<= x b))))
+
+(defn group-by-n [n x]
+  (defn go [i output input]
+    (if (empty? input)
+      output
+      (let [[head & tail] input]
+        (if (= i 0)
+          (go 1 (cons (list head) output) tail)
+          (let
+            [[ohead & otail] output]
+            (go
+              (mod (+ i 1) n)
+              (cons (cons head ohead) otail)
+              tail)))))
+    )
+  (reverse (map reverse (go 0 '() x))))
