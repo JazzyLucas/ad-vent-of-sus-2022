fa52d190b43acabbe7a1877c61aadd104e33ee81
Jonathan Lemos <jonathanlemos@protonmail.com>
2022-12-16 18:11:29 -0500

    day 16 part 2

--- scripts/day16.py
+++ scripts/day16.py
@@ -1,5 +1,5 @@
 from functools import cache
-from itertools import combinations, product
+from itertools import combinations, product, chain
 
 from common import *
 
@@ -32,36 +32,31 @@ for input_file in ["16a", "16b"]:
         return dp("AA", 30, frozenset())
 
     def part2():
+        actual_valves = frozenset(v for v in valves if valves[v][0] > 0)
+
         @cache
-        def dp(human, elephant, minutes, open):
+        def dp(valve, minutes, open):
             if minutes <= 1:
                 return 0
 
             best = 0
-            for human_neighbor, elephant_neighbor in product(valves[human][1], valves[elephant][1]):
-                if human not in open and valves[human][0] > 0 and elephant not in open and valves[elephant][0] > 0 and human != elephant:
-                    best = max(
-                        best,
-                        (minutes - 1) * valves[human][0] + (minutes - 1) * valves[elephant][0] +
-                        dp(human, elephant, minutes - 1, open | {human, elephant})
-                    )
-                if human not in open and valves[human][0] > 0:
-                    best = max(
-                        best,
-                        (minutes - 1) * valves[human][0] +
-                        dp(human, elephant_neighbor, minutes - 1, open | {human})
-                    )
-                if elephant not in open and valves[elephant][0] > 0:
-                    best = max(
-                        best,
-                        (minutes - 1) * valves[elephant][0] +
-                        dp(human_neighbor, elephant, minutes - 1, open | {elephant})
-                    )
-                best = max(best, dp(human_neighbor, elephant_neighbor, minutes - 1, open))
+            for neighbor in valves[valve][1]:
+                if valve not in open and valves[valve][0] > 0:
+                    best = max(best, (minutes - 1) * valves[valve][0] + dp(neighbor, minutes - 2, open | {valve}))
+                best = max(best, dp(neighbor, minutes - 1, open))
 
             return best
 
-        return dp("AA", "AA", 26, frozenset())
+        def powerset(iterable):
+            s = list(iterable)
+            return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+        best = 0
+        for i, subset in enumerate(frozenset(x) for x in powerset(actual_valves)):
+            if i % 10 == 0:
+                print(i)
+            best = max(best, dp("AA", 26, subset) + dp("AA", 26, frozenset(actual_valves - subset)))
+        return best
 
 
     print(input_file, part2())
