f0e5901b5849365e44bcf190069061b76046eef5
Jonathan Lemos <jonathanlemos@protonmail.com>
2022-12-18 14:46:22 -0500

    day 18 not working 2

--- scripts/day18.py
+++ scripts/day18.py
@@ -6,7 +6,7 @@ from itertools import *
 from functools import *
 import re
 
-for input_file in ["18a2", "18b", "18a"]:
+for input_file in ["18a", "18a2", "18b"]:
     ls = lines(aoc_input(input_file))
     triples = [tuple(int(x) for x in line.split(",")) for line in ls]
 
@@ -48,157 +48,108 @@ for input_file in ["18a2", "18b", "18a"]:
         return total - adj
 
 
-    def surface_area_of(triples):
-        abs = defaultdict(list)
-        bcs = defaultdict(list)
-        acs = defaultdict(list)
-        for x, y, z in triples:
-            abs[(x, y)].append(z)
-            acs[(x, z)].append(y)
-            bcs[(y, z)].append(x)
-
-        for x in abs:
-            abs[x] = sorted(abs[x])
-        for x in acs:
-            acs[x] = sorted(acs[x])
-        for x in bcs:
-            bcs[x] = sorted(bcs[x])
-
-        total = len(triples) * 6
-        adj = 0
-
-        for x in abs:
-            for a, b in zip(abs[x][1:], abs[x][:-1]):
-                if a - b == 1:
-                    adj += 2
-
-        for x in bcs:
-            for a, b in zip(bcs[x][1:], bcs[x][:-1]):
-                if a - b == 1:
-                    adj += 2
-
-        for x in acs:
-            for a, b in zip(acs[x][1:], acs[x][:-1]):
-                if a - b == 1:
-                    adj += 2
-
-        return total - adj
-
-    def filter_surrounded(triples):
-        ret = set()
-
-        abs = defaultdict(list)
-        bcs = defaultdict(list)
-        acs = defaultdict(list)
-
-        for x, y, z in triples:
-            abs[(x, y)].append(z)
-            acs[(x, z)].append(y)
-            bcs[(y, z)].append(x)
-
-        for x in abs:
-            abs[x] = sorted(abs[x])
-        for x in acs:
-            acs[x] = sorted(acs[x])
-        for x in bcs:
-            bcs[x] = sorted(bcs[x])
-
-        def in_range(l, x):
-            return len(l) > 0 and l[0] < x < l[-1]
-
-        def surrounded(x, y, z):
-            return in_range(abs[(x, y)], z) and in_range(acs[(x, z)], y) and in_range(bcs[(y, z)], x)
-
-        for x in abs:
-            a, b = x
-            for c in abs[x]:
-                if surrounded(a, b, c):
-                    ret.add((a, b, c))
-
-        for x in bcs:
-            b, c = x
-            for a in bcs[x]:
-                if surrounded(a, b, c):
-                    ret.add((a, b, c))
-
-        for x in acs:
-            a, c = x
-            for b in acs[x]:
-                if surrounded(a, b, c):
-                    ret.add((a, b, c))
-        return set(triples) - ret
-
-    def exterior_surface_area_of(triples):
-        if len(triples) == 0:
-            return 0
-
-        abs = defaultdict(list)
-        bcs = defaultdict(list)
-        acs = defaultdict(list)
-
-        for x, y, z in triples:
-            abs[(x, y)].append(z)
-            acs[(x, z)].append(y)
-            bcs[(y, z)].append(x)
-
-        for x in abs:
-            abs[x] = sorted(abs[x])
-        for x in acs:
-            acs[x] = sorted(acs[x])
-        for x in bcs:
-            bcs[x] = sorted(bcs[x])
-
-        total = len(triples) * 6
-        adj = 0
-
-        air_pockets = set()
-
-        def in_range(l, x):
-            return len(l) > 0 and l[0] < x < l[-1]
-
-        def surrounded(x, y, z):
-            return in_range(abs[(x, y)], z) and in_range(acs[(x, z)], y) and in_range(bcs[(y, z)], x)
-
-        def exists(l, x):
-            return l[bisect.bisect_left(l, x)] == x
-
-        for x in abs:
-            if len(abs[x]) == 0:
-                continue
-            a, b = x
-            for i in range(abs[x][0], abs[x][-1] + 1):
-                if exists(abs[x], i):
-                    if exists(abs[x], i - 1):
-                        adj += 2
-                elif surrounded(a, b, i):
-                    air_pockets.add((a, b, i))
-
-        for x in bcs:
-            if len(bcs[x]) == 0:
-                continue
-            b, c = x
-            for i in range(bcs[x][0], bcs[x][-1] + 1):
-                if exists(bcs[x], i):
-                    if exists(bcs[x], i - 1):
-                        adj += 2
-                elif surrounded(i, b, c):
-                    air_pockets.add((i, b, c))
-
-        for x in acs:
-            if len(acs[x]) == 0:
-                continue
-            a, c = x
-            for i in range(acs[x][0], acs[x][-1] + 1):
-                if exists(acs[x], i):
-                    if exists(acs[x], i - 1):
-                        adj += 2
-                elif surrounded(a, i, c):
-                    air_pockets.add((a, i, c))
-
-        return total - adj - exterior_surface_area_of(filter_surrounded(air_pockets))
-
     def part2():
-        return exterior_surface_area_of(filter_surrounded(triples))
+        rects = set()
+        for x, y, z in triples:
+            rects.add(frozenset([(x, y, z), (x + 1, y, z), (x, y + 1, z), (x + 1, y + 1, z)]))
+            rects.add(frozenset([(x, y, z), (x + 1, y, z), (x, y, z + 1), (x + 1, y, z + 1)]))
+            rects.add(frozenset([(x, y, z), (x, y + 1, z), (x, y, z + 1), (x, y + 1, z + 1)]))
+            rects.add(frozenset([(x + 1, y + 1, z + 1), (x, y + 1, z + 1), (x + 1, y, z + 1), (x, y, z + 1)]))
+            rects.add(frozenset([(x + 1, y + 1, z + 1), (x, y + 1, z + 1), (x + 1, y + 1, z), (x, y + 1, z)]))
+            rects.add(frozenset([(x + 1, y + 1, z + 1), (x + 1, y, z + 1), (x + 1, y + 1, z), (x + 1, y, z)]))
+
+        def x_set(rect):
+            return frozenset(x for x, y, z in rect)
+
+        def y_set(rect):
+            return frozenset(y for x, y, z in rect)
+
+        def z_set(rect):
+            return frozenset(z for x, y, z in rect)
+
+        def orientation(rect):
+            if len(x_set(rect)) == 1:
+                return "x"
+            elif len(y_set(rect)) == 1:
+                return "y"
+            else:
+                return "z"
+
+        def get_orientation(o):
+            if o == "x":
+                return lambda x: x[0]
+            elif o == "y":
+                return lambda x: x[1]
+            else:
+                return lambda x: x[2]
+
+        def mutate_orientation(o, f):
+            if o == "x":
+                return lambda x: (f(x[0]), x[1], x[2])
+            elif o == "y":
+                return lambda x: (x[0], f(x[1]), x[2])
+            else:
+                return lambda x: (x[0], x[1], f(x[2]))
+
+        def get_orientation_complement(o):
+            if o == "x":
+                return lambda x: (x[1], x[2])
+            elif o == "y":
+                return lambda x: (x[0], x[2])
+            else:
+                return lambda x: (x[0], x[1])
+
+        def edges(rect):
+            return [(a, b) for a, b in permutations(rect, 2) if sum(1 for c, d in zip(a, b) if c == d) == 2]
+
+        def fan(edge, axis):
+            a, b = edge
+            ret = []
+            for delta in (-1, 1):
+                ret.append(frozenset([
+                    a,
+                    b,
+                    mutate_orientation(axis, lambda x: x + delta)(a),
+                    mutate_orientation(axis, lambda x: x + delta)(b)
+                ]))
+            return ret
+
+        def neighbors(rect, up_is_positive):
+            es = edges(rect)
+
+            plus_x = frozenset((x + 1, y, z) for x, y, z in rect)
+            minus_x = frozenset((x - 1, y, z) for x, y, z in rect)
+            plus_y = frozenset((x, y + 1, z) for x, y, z in rect)
+            minus_y = frozenset((x, y - 1, z) for x, y, z in rect)
+            plus_z = frozenset((x, y, z + 1) for x, y, z in rect)
+            minus_z = frozenset((x, y, z + 1) for x, y, z in rect)
+
+            rs = [plus_x, minus_x, plus_y, minus_y, plus_z, minus_z]
+            pluses = [plus_x, plus_y, plus_z]
+            minuses = [minus_x, minus_y, minus_z]
+
+            ret = []
+
+            for edge in es:
+                candidates = [r for r in rs if edge in r]
+                assert len(candidates) == 1
+                a, b = fan(edge, orientation(rect))
+                c = [a, candidates[0], b]
+                if up_is_positive == candidates[0] in pluses:
+                    c = c[::-1]
+                ret.append(c)
+
+            return ret
+
+        seen = set()
+        def dfs(rect, up_is_positive):
+
+
+
+
+
+        return len(new_rects)
+
 
 
     print(input_file, part2())
