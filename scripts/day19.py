ac07409294ecfe26964db756f0230c5a3bd20f1d
Jonathan Lemos <jonathanlemos@protonmail.com>
2022-12-19 21:38:09 -0500

    painfully slow day 19

--- scripts/day19.py
+++ scripts/day19.py
@@ -51,7 +51,7 @@ for input_file in ["19a", "19b"]:
                                          obsidian_count + obsidian_bots,
                                          geode_bots,
                                          minutes - 1))
-                if ore_count >= clay_ore_cost and clay_count < obsidian_clay_cost and clay_bots < obsidian_clay_cost:
+                if ore_count >= clay_ore_cost and clay_bots < obsidian_clay_cost:
                     best = max(best, dfs(ore_bots,
                                          ore_count - clay_ore_cost + ore_bots,
                                          clay_bots + 1,
@@ -60,15 +60,15 @@ for input_file in ["19a", "19b"]:
                                          obsidian_count + obsidian_bots,
                                          geode_bots,
                                          minutes - 1))
-                if best == 0:
-                    best = dfs(ore_bots,
+                if True or best == 0:
+                    best = max(best, dfs(ore_bots,
                                ore_count + ore_bots,
                                clay_bots,
                                clay_count + clay_bots,
                                obsidian_bots,
                                obsidian_count + obsidian_bots,
                                geode_bots,
-                               minutes - 1)
+                               minutes - 1))
 
                 return geode_bots + best
 
@@ -77,7 +77,81 @@ for input_file in ["19a", "19b"]:
 
 
     def part2():
-        pass
+        quality = []
+        for i in range(min(len(ns), 3)):
+            ore_ore_cost, clay_ore_cost, obsidian_ore_cost, obsidian_clay_cost, geode_ore_cost, geode_obsidian_cost = \
+                ns[i]
+
+            max_ore_cost = max(ore_ore_cost, clay_ore_cost, obsidian_ore_cost, geode_ore_cost)
+
+            @cache
+            def dfs(ore_bots, ore_count, clay_bots, clay_count, obsidian_bots, obsidian_count, geode_bots, minutes):
+                if minutes == 0:
+                    return 0
+
+                need_ore = minutes * (max_ore_cost - ore_bots) > ore_count
+                need_clay = minutes * (obsidian_clay_cost - clay_bots) > clay_count
+                need_obsidian = minutes * (geode_obsidian_cost - obsidian_bots) > obsidian_count
+
+                obsidian_will_pay_off = need_obsidian
+                clay_will_pay_off = need_clay
+                ore_will_pay_off = (minutes > ore_ore_cost or True) and need_ore
+
+                can_make_geode = ore_count >= geode_ore_cost and obsidian_count >= geode_obsidian_cost
+                can_make_obsidian = ore_count >= obsidian_ore_cost and clay_count >= obsidian_clay_cost and obsidian_will_pay_off
+                can_make_ore = ore_count >= ore_ore_cost and ore_will_pay_off
+                can_make_clay = ore_count >= clay_ore_cost and clay_will_pay_off
+
+                best = 0
+                if can_make_geode:
+                    best = max(best, dfs(ore_bots,
+                                         ore_count + ore_bots - geode_ore_cost,
+                                         clay_bots,
+                                         clay_count + clay_bots,
+                                         obsidian_bots,
+                                         obsidian_count - geode_obsidian_cost + obsidian_bots,
+                                         geode_bots + 1,
+                                         minutes - 1))
+                if can_make_obsidian:
+                    best = max(best, dfs(ore_bots,
+                                         ore_count - obsidian_ore_cost + ore_bots,
+                                         clay_bots,
+                                         clay_count - obsidian_clay_cost + clay_bots,
+                                         obsidian_bots + 1,
+                                         obsidian_count + obsidian_bots,
+                                         geode_bots,
+                                         minutes - 1))
+                if can_make_ore:
+                    best = max(best, dfs(ore_bots + 1,
+                                         ore_count - ore_ore_cost + ore_bots,
+                                         clay_bots,
+                                         clay_count + clay_bots,
+                                         obsidian_bots,
+                                         obsidian_count + obsidian_bots,
+                                         geode_bots,
+                                         minutes - 1))
+                if can_make_clay:
+                    best = max(best, dfs(ore_bots,
+                                         ore_count - clay_ore_cost + ore_bots,
+                                         clay_bots + 1,
+                                         clay_count + clay_bots,
+                                         obsidian_bots,
+                                         obsidian_count + obsidian_bots,
+                                         geode_bots,
+                                         minutes - 1))
+                best = max(best, dfs(ore_bots,
+                                     ore_count + ore_bots,
+                                     clay_bots,
+                                     clay_count + clay_bots,
+                                     obsidian_bots,
+                                     obsidian_count + obsidian_bots,
+                                     geode_bots,
+                                     minutes - 1))
+
+                return geode_bots + best
+
+            quality.append(dfs(1, 0, 0, 0, 0, 0, 0, 32))
+        return reduce(lambda a, c: a * c, quality)
 
 
-    print(input_file, part1())
+    print(input_file, part2())
